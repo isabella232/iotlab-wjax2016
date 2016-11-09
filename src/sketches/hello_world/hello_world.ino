b9db17e7605dc5d6bfa97915bf4b1619947ef21b
J. Delgado <jan.p.delgado.kramer@accenture.com>
2016-11-09 01:17:42 +0100

    initial release of sketches

---
+++ src/sketches/hello_world/hello_world.ino
@@ -0,0 +1,18 @@
+/* 
+ * IoT-Lab: "hello, World!", embedded version.
+ * let the built-in LED of the NodeMCU board blink
+ */
+#include <pins_arduino.h>
+
+const int LED_PIN = BUILTIN_LED;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+}
